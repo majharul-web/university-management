a1b9fb9c730d515ac1fe63b656e3e1d129eccf20
majharul <majharuljony163@gmail.com>
2023-05-29 10:46:53 +0600

    [DONE] setup mongoose express app

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,15 @@
 import express, { Application } from "express";
+import cors from "cors";
 
 const app: Application = express();
+
+// Middleware
+app.use(cors());
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.send("Hello World");
+});
 
 export default app;

3822ca46117c16276a9b6476f97935e5192a2536
majharul <majharuljony163@gmail.com>
2023-05-29 10:15:32 +0600

    express project setup and database connection done

---
+++ src/server.ts
@@ -0,0 +1,17 @@
+import mongoose from "mongoose";
+import app from "./app";
+import config from "./config";
+
+async function bootstrap() {
+  try {
+    await mongoose.connect(config.database_url as string);
+    console.log("Connected to MongoDB");
+    app.listen(config.port, () => {
+      console.log(`Server is listening on port ${config.port}`);
+    });
+  } catch (error) {
+    console.log("Error connecting to MongoDB: ", error);
+  }
+}
+
+bootstrap();
